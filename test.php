3713c11c993a039128d785c3edc33494216ad4b6
genautkin <genautkin@gmail.com>
2022-01-09 21:04:45 +0200

    Form

--- test.php
+++ test.php
@@ -1,28 +1,71 @@
+<?php 
+ 
+$for_time = time() + 60 * 60 * 24 * 30;
+setcookie('stay_connected', 1, $for_time);
+
+?>
+<?php
+$user = [
+    'name' => 'Avi Cohen',
+    'age' => 26,
+    'email' => 'avi@gmail.com',
+    '0' => 'test'
+];
+ 
+?>
+ 
 <!DOCTYPE html>
-<html lang="en">
-<head>
+<html>
+  <head>
+    <title></title>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php 
+  </head>
+  <body>
+    <h2>- User details -</h2>
+    <ul>
+      <li>Name: <?= $user['name']; ?></li>
+      <li>Age: <?= $user['age']; ?></li>
+      <li>Email: <?= $user['email']; ?></li>
+      <?php
  
+ $names1 = [
+     'a' => 'avi',
+     'b' => 'beni',
+ ];
+  
+ $names2 = [
+     'a' => 'carl',
+     'd' => 'dana',
+ ];
+  
+ $all_names = array_merge($names1, $names2, $user);
+  
+ $names = [
+  'a' => 'avi',
+  'b' => 'beni',
+  'c' => 'carl',
+  'd' => 'dana',
+];
 
- 
- $num = 6;
-  
- $adder = function() use(&$num){
-   $num += 2;
-    
- };
-  
- $adder();
-  
+function my_add($n)
+{
+    return $n.' !';
+}
 
- echo $num;
+
+$result = array_map(fn($value): string => $value.' !', $names);
+
+echo '<pre>';
+print_r($result);
+echo '</pre>';
+
+echo '<pre>';
+print_r($names);
+echo '</pre>';
  ?>
-
+    </ul>
+  </body>
+</html>
+    
 </body>
 </html>
